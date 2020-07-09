b62a821926c4791ecb5524aa5683b84f339fd6cf
masterdrue <masterdrue@gmail.com>
2020-07-08 22:31:52 -0400

    npm watch

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+/* Carousel Button */
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
+
+/* Reserve Campsite Modal Form */
+function reserve() {
+    const message = document.querySelector("#message");
+    const checkedInput = document.querySelector("input:checked");
+    const date = document.getElementById("date");
+    const numCampers = document.querySelector("select");
+
+         if (checkedInput) {
+            message.textContent = `Your Reservation is confirmed for ${date.value}, and includes ${checkedInput.value} with ${numCampers.value} Campers.`;
+        } else {
+        message.textContent - "Unknown reserveration.";
+        }
+    }
+/* ToolTip */
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
